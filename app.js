0ae790a9480f52d9d5cfe1a7df95b2d56acaba5e
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-05-28 18:16:56 +0300

    initialize cell array font to be later on styled losing the game and starting the game

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ var ctx = canvas.getContext("2d");
 var sizeInput = document.getElementById("size");
 var changeSize = document.getElementById("change-size");
 var scoreLabel = document.getElementById("score");
+
+var score = 0;
+var size = 4;
+var width = canvas.width / size - 6;
+
+var cells = [];
+var fontSize;
+var loss = false;
+startGame();
