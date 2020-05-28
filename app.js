3fdfbd716b5d8e6f1626ad03ee6b6df807616ccf
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-05-28 18:14:33 +0300

    initialize grid size, score and width

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+
+var sizeInput = document.getElementById("size");
+var changeSize = document.getElementById("change-size");
+var scoreLabel = document.getElementById("score");
