e76e4bce135b7d91cf01eca6ad788151eb23a221
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-05-28 19:48:44 +0300

    moveUp function gets new number on the grid when the up kep is pushed

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ startGame();
 function startGame() {
   createCells();
   drawAllCells();
+  pasteNewCell();
 }
 
 function cell(row, coll) {
@@ -149,3 +150,29 @@ document.onkeydown = function (event) {
     scoreLabel.innerHTML = "Score : " + score;
   }
 };
+
+function moveUp() {
+  var i, j, row;
+  for (j = 0; j < size; j++) {
+    for (i = 1; i < size; i++) {
+      if (cells[i][j].value) {
+        row = i;
+        while (row > 0) {
+          if (!cells[row - 1][j].value) {
+            cells[row - 1][j].value = cells[row][j].value;
+            cells[row][j].value = 0;
+            row--;
+          } else if (cells[row][j].value == cells[row - 1][j].value) {
+            cells[row - 1][j].value *= 2;
+            score += cells[row - 1][j].value;
+            cells[row][j].value = 0;
+            break;
+          } else {
+            break;
+          }
+        }
+      }
+    }
+  }
+  pasteNewCell();
+}
